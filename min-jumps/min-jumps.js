f5058cf521f5adf344a99a91b07b15adc5159ddd
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-10-27 15:51:56 -0500

    complete

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+
+// 1. start at beginning of array
+// 2. loop through possible number of jumps allowed in that space
+// 3. for each jump length, recursively pass in the array of the spot you would land on
+// 4. track total distance, base case is when you pass the end length of the array
 
 // Complete this algo
-const minJumps = arr => {
-
+const minJumps = (arr) => {
+  const jumpRange = arr[0];
+  let numJumps = [];
+  for (let i = 1; i < jumpRange + 1; i++) {
+    if (i >= arr.length - 1) {
+      numJumps.push(0);
+    } else {
+      numJumps.push(minJumps([...arr.slice(i)]));
+    }
+  }
+  return 1 + Math.min(...numJumps);
 };
 
-module.exports = minJumps
+module.exports = minJumps;
