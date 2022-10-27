2c7f32fcc9d8e4deadda45165fd078e76f395097
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-10-27 14:06:20 -0500

    binary search complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, start = 0, end = array.length - 1) => {
+  // 0. base case: indicies are next to eachother (ie no midpoint) -> return false
+  // 1. find midpoint index value
+  // 2. compare midpoint array value to target (lower than, equal to, or higher than)
+  // 3. if equal to, return true
+  //   if lower than, take lower half of the array and enter into the function recursively
+  //  same for higher than but opposite
+
+  //   if (!end) {
+  // 	end = array.length -1
+  //   }
+  if (array[start] === array[end]) {
+    if (array[start] === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const midIndex = Math.floor((start + end) / 2);
+  //console.log(start, midIndex, end);
+  if (array[midIndex] === target) {
+    return true;
+  } else if (array[midIndex] > target) {
+    return binarySearch(array, target, start, midIndex);
+  } else {
+    return binarySearch(array, target, midIndex + 1, end);
+  }
 };
 
 /*
@@ -14,4 +39,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
