faeb38744faacd464110f5c1b2c1794aecb362a6
SohanMahmud <sohanmahmud@yahoo.com>
2022-10-27 16:02:41 -0400

    finished isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,41 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  //1. while loop
+  //2. first runner travels n nodes
+  //3. 2nd runner travels 2n nodes
+  //4. compare values, if same return value
+  //5. if not same increment n
+  let runner1;
+  let runner2;
+  let n = 0;
 
+  if (linkedlist.tail.next !== null) {
+    return true;
+  }
+  let current = linkedlist.head;
+  while (current.next !== null) {
+    try {
+      runner1 = linkedlist.getNthNode(n + 1);
+
+      runner2 = linkedlist.getNthNode(n * 2 + 2);
+
+      console.log(n, runner1, runner2);
+      if (runner1 === runner2) {
+        console.log("hit if");
+        return true;
+      }
+
+      n++;
+      current = current.next;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +45,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
